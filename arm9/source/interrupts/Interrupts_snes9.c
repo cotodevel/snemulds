2659e15079e548c3814ea50527601fa2d3a7ae8c
Coto <programmer.coto.1988@gmail.com>
2020-05-05 13:24:36 -0400

    fix console bits

--- arm9/source/interrupts/Interrupts_snes9.c
+++ arm9/source/interrupts/Interrupts_snes9.c
@@ -133,7 +133,10 @@ __attribute__((section(".itcm")))
 #endif
 inline __attribute__((always_inline)) 
 void screenLidHasOpenedhandlerUser(){
-	setBacklight(POWMAN_BACKLIGHT_TOP_BIT | POWMAN_BACKLIGHT_BOTTOM_BIT);	//both lit screens
+	//if console top screen, shut off console
+	if(GUI.consoleAtTopScreen == true){
+		detectAndTurnOffConsole();
+	}
 }
 
 //Note: this event is hardware triggered from ARM7, on ARM9 a signal is raised through the FIFO hardware
