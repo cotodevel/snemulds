4e9f81e5e6101b3e7a18cbbf52f0a62d0498a648
Coto <programmer.coto.1988@gmail.com>
2021-11-14 04:47:31 -0300

    - add ability to switch between legacy NTR or TWL touchscreen input reads from within the TGDS project - fix Woopsi SDK touchscreen input again so it works in TWL mode - SnemulDS TWL mode toggles TSC modes now

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -604,6 +604,36 @@ int main(int argc, char ** argv){
 	memset(&guiSelItem, 0, sizeof(guiSelItem));
 	guiSelItem.StructFDFromFS_getDirectoryListMethod = FT_FILE;
 	
+	//Choose Touchscreen controller mode if TWL mode
+	if(__dsimode == true){
+		//Send args
+		printf("TWL Mode detected. Want to use TWL touchscreen?");
+		printf("(X) Yes. ");
+		printf("(Y) No. (Legacy NTR touchscreen) ");
+		
+		while(1==1){
+			scanKeys();
+			if(keysDown()&KEY_X){
+				scanKeys();
+				while(keysHeld() & KEY_X){
+					scanKeys();
+				}
+				//SnemulDS -> use TWL TSC
+				TWLSetTouchscreenTWLMode();
+				break;
+			}
+			if(keysDown()&KEY_Y){
+				scanKeys();
+				while(keysHeld() & KEY_Y){
+					scanKeys();
+				}
+				//TGDS-Projects -> legacy NTR TSC compatibility
+				TWLSetTouchscreenNTRMode();
+				break;
+			}
+		}
+	}
+	
 	//ARGV Support: 
 	if (argc > 1) {
 		strcpy(&CFG.ROMFile[0], (const char *)argv[1]);
